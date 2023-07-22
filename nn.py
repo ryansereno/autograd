61dfd46b3de9cb959addf055424be0a3ca4c32ea
Ryan <ryan@Ryans-MacBook-Pro.local>
2023-07-22 19:21:07 -0400

    corrected layer output to prevent list output

--- nn.py
+++ nn.py
@@ -20,7 +20,7 @@ class Layer:
 
   def __call__(self,x):
     outs = [n(x) for n in self.neurons]
-    return outs
+    return outs[0] if len(outs) == 1 else outs
 
 class MLP:
   def __init__(self, nin, nouts):
